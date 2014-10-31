d7492c76312df549b0b18543281f281618ae2bc6
Luminus Olumide Alabi <luminus@odin.local>
2014-10-31 15:04:53 +0100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,24 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
+    
 }
 
 -(void)update:(CCTime)delta
 {
-    // put update code here
+    // increment the time since the last obstacle was added
+    timeSinceObstacle += delta; //delta is aproximately 1/60th of a second
+    
+    // Check to see if 2 seconds have passed
+    if (timeSinceObstacle > 2.0f) {
+
+        // Add a new obstacle
+        [self addObstacle];
+        
+        // Then rest the timer
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 // put new methods here
